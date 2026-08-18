224d94de602578ce690acc02bee1b5136b298ace
agrimkapoor <agrimkapoor07@gmail.com>
2026-08-18 12:00:00 +0530

    Update index.js

--- backend/services/chat/index.js
+++ backend/services/chat/index.js
@@ -1,1 +1,23 @@
+import express from "express";
+import dotenv from "dotenv";
 
+import router from "./routes/chat.routes.js";
+import connectDB from "./config/db.js";
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT;
+
+// Middleware
+app.use(express.json());
+
+// Routes
+app.use("/", router);
+
+// Start server
+app.listen(port, () => {
+  connectDB();
+
+  console.log(`Chat service running on port ${port}`);
+});
