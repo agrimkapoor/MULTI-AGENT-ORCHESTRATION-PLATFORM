0d91cf7983b2b6617ef00a7610cc5b7c3d1d9610
agrimkapoor <agrimkapoor07@gmail.com>
2026-08-18 18:25:04 +0530

    Set up billing service with Express and middleware

--- backend/services/billing/index.js
+++ backend/services/billing/index.js
@@ -1,1 +1,36 @@
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
+import dotenv from "dotenv";
 
+import connectDB from "./config/db.js";
+import router from "./routes/billing.routes.js";
+
+dotenv.config();
+
+const port = process.env.PORT;
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("dev"));//logs request in terminal
+
+// Routes
+app.use("/", router);
+
+app.get("/", (req, res) => {
+  res.json({
+    success: true,
+    message: "Billing Service Running",
+  });
+});
+
+// Start server
+app.listen(port, () => {
+  connectDB();
+
+  console.log(`Billing service running on ${port}`);
+});
