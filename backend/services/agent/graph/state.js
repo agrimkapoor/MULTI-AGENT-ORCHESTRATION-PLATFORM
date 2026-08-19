25f15b05ef57d190b70d2e0b834944be9ae27732
agrimkapoor <agrimkapoor07@gmail.com>
2026-08-19 16:43:07 +0530

    Update state.js

--- backend/services/agent/graph/state.js
+++ backend/services/agent/graph/state.js
@@ -1,27 +1,23 @@
 import { Annotation } from "@langchain/langgraph";
 
+//defining agent state
 export const AgentState = Annotation.Root({
   prompt: Annotation(),
-
   conversationId: Annotation(),
-
   userId: Annotation(),
-
   agent: Annotation(),
-
   response: Annotation(),
-
   images: Annotation(),
-
   model: Annotation(),
-
   file: Annotation(),
-
   artifacts: Annotation(),
-
   searchResults: Annotation(),
-
   codeContext: Annotation(),
-
-  pdfContext: Annotation(),
+  pdfContext: Annotation()
 });
+
+/*
+  When an agent performs multiple steps (e.g., search → summarize → generate answer), each step needs access to data from previous steps. The state acts as a shared memory across nodes in the LangGraph graph.
+
+  Instead of passing many parameters between functions, all nodes read from and write to the same AgentState object.
+*/
